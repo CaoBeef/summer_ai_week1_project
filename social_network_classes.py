d2f20b5e5363b494aaa478ad63c5a2805884711f
Hongjin Cao <caobeef@MacBook-Pro-12.local>
2023-07-17 22:09:00 -0400

    i added a new file 2

--- social_network_classes.py
+++ social_network_classes.py
@@ -1,27 +1,29 @@
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
-class SocialNetwork:
-    def __init__(self):
-        self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
+SocialNetwork = []
+#class SocialNetwork:
+
+ #   def __init__(self):
+  #      self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
                                  # you can save objects of people on the network in this list
         
     ## For more challenge try this
-    def save_social_media(self):
+   # def save_social_media(self):
         # function to save social media to a file on disk 
         # hint: look up how to use python's inbuil json module to turn objects to json
         # you can write this json unto a file on disk
-        pass
+    #    pass
 
     ## For more challenge try this
-    def reload_social_media(self):
+    #def reload_social_media(self):
         # function to load saved social media from file on disk 
         # hint: load a the json file from disk and look up how to recreate the list of people objects.
-        pass
+     #   pass
 
-    def  create_account(self,new_name,new_age,new_password,new_friendlist,new_received_messages,new_sent_messages):
-        SocialNetwork.append(Person(new_name,new_age,new_password,[],[],[]))
+    #def  create_account(self,new_name,new_age,new_password,new_friendlist,new_received_messages,new_sent_messages):
+     #   SocialNetwork.append(Person(new_name,new_age,new_password,[],[],[]))
         #implement function that creates account here
-        print("Creating ...")
-        pass
+       # print("Creating ...")
+      #  pass
 
 
 class Person():
