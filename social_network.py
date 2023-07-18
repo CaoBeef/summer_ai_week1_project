52549de40a734f9ebb136c44372cb5a9134c68ce
Hongjin Cao <caobeef@MacBook-Pro-12.local>
2023-07-17 22:15:18 -0400

    i'm done

--- social_network.py
+++ social_network.py
@@ -1,155 +1,144 @@
-from social_network_classes import SocialNetwork, Person
+#Various import Statements can go here
+from  social_network_classes import social_network,Person
 import social_network_ui
 
-ai_social_network = SocialNetwork()
 
+
+#Create instance of main social network object
+
+
+#The line below is a python keyword to specify which 
 if __name__ == "__main__":
     print("########################################################")
     print("          Welcome to Summer AI Social Network")
     print("########################################################")
-
-    while True:
-        choice = social_network_ui.mainMenu()
-
-        if choice == '1':
-            print("\nYou are now in the create account menu")
-            ai_social_network.create_account()
-
-        elif choice == '2':
-            inner_menu_choice = social_network_ui.manageAccountMenu()
-            if login_bool == True:
-                manage_choice = social_network_ui.manageAccountMenu()
-                # Handle inner menu here
-            else:
-                continue
-            if manage_choice == '8':
-                continue
-            else:
-                if manage_choice == '1':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    updated_userid = input("What would you like your new user ID to be? ")
-                    updated_age = int(input("What would you like your new age to be? "))
-                    updated_password = input("What would you like your new password to be? ")
-                    updated_user_found = False
-                    for person in ai_social_network:
-                        if person.user_id == updated_userid:
-                            print("This username is already in use!")
+    last_menu = None
+    #restart menu
+while True:
+    choice = social_network_ui.mainMenu()
+    if choice == '1':
+        social_network_ui.createAccount()
+    elif choice == '2':
+        login_bool = social_network_ui.userLogin()
+        if login_bool == True:
+            manage_choice = social_network_ui.manageAccountMenu()
+             #Handle inner menu here
+        else:
+            continue
+        if manage_choice == '8':
+            continue
+        else:
+            if manage_choice == '1':
+                current_userid = input("What is your name? ")
+                current_password = input("What is your password? ")
+                updated_userid = input("What would you like your new name to be? ")
+                updated_age = int(input("What would you like your new age to be? "))
+                updated_password = input("What would you like your new password to be? ")
+                updated_user_found = False
+                for person in social_network:
+                    if (person.user_id == updated_userid):
+                        print("This username is already in use!")
+                        updated_user_found = True
+                while not updated_user_found:
+                    for index in range(len(social_network)):
+                        if (social_network[index].user_id == current_userid) and (social_network[index].password == current_password):
+                            social_network[index] = Person(updated_userid,updated_age,updated_password,social_network[index].friendlist,social_network[index].receivedmessages,social_network[index].sentmessages)
+                            print("User info successfuly updated!") 
                             updated_user_found = True
                             break
-                    if not updated_user_found:
-                        for index in range(len(ai_social_network)):
-                            if (ai_social_network[index].user_id == current_userid) and (ai_social_network[index].password == current_password):
-                                ai_social_network[index] = Person(updated_userid, updated_age, updated_password, ai_social_network[index].friendlist, ai_social_network[index].receivedmessages, ai_social_network[index].sentmessages)
-                                print("User info successfully updated!")
-                                updated_user_found = True
+                    if updated_user_found == False:
+                        print("Couldn't update user credentials!")
+                        updated_user_found = True
+                        break
+            if manage_choice == '2':
+                current_userid = input("What is your username? ")
+                current_password = input("What is your password? ")
+                new_friend = input("What is the username of the friend you'd like to add? ")
+                user_exist = True
+                for person1 in social_network:
+                    if (person1.user_id == current_userid) and (person1.password == current_password):
+                        user_exist = False
+                        current_object = person1
+                        current_index = social_network.index(person1)
+                if user_exist == True:
+                    print("Incorrect credentials!")
+                while not user_exist:
+                    for person2 in social_network:
+                        if (person2.user_id not in current_object.friendlist):
+                            if (person2.user_id == new_friend):
+                                person1.add_friend(new_friend)
+                                social_network[current_index] = person1
+                                print("Friend added successfully!")
+                                user_exist = True
                                 break
-                        if not updated_user_found:
-                            print("Couldn't update user credentials!")
-
-                elif manage_choice == '2':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    new_friend = input("What is the username of the friend you'd like to add? ")
-                    user_exist = False
-                    current_object = None
-                    current_index = None
-                    for person1 in ai_social_network:
-                        if (person1.user_id == current_userid) and (person1.password == current_password):
-                            user_exist = True
-                            current_object = person1
-                            current_index = ai_social_network.index(person1)
-                            break
-                    if not user_exist:
-                        print("Incorrect credentials!")
-                    else:
-                        friend_exist = False
-                        for person2 in ai_social_network:
-                            if person2.user_id == new_friend:
-                                if person2.user_id not in current_object.friendlist:
-                                    current_object.add_friend(new_friend)
-                                    ai_social_network[current_index] = current_object
-                                    print("Friend added successfully!")
-                                else:
-                                    print("Friend already added!")
-                                friend_exist = True
-                                break
-                        if not friend_exist:
-                            print("Error! Make sure the friend exists!")
-                
-                elif manage_choice == '3':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    found_object = False
-                    for person3 in ai_social_network:
-                        if (person3.user_id == current_userid) and (person3.password == current_password):
-                            for element in person3.friendlist:
-                                print(element)
-                            found_object = True
-                            break
-                    if not found_object:
-                        print("Couldn't find user!")
-                
-                elif manage_choice == '4':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    blocked_friend = input("What is the username of the user you'd like to block? ")
-                    found_blocked = False
-                    for index2, person in enumerate(ai_social_network):
-                        if (person.user_id == current_userid) and (person.password == current_password):
-                            if blocked_friend in person.friendlist:
-                                person.friendlist.remove(blocked_friend)
+                    if user_exist == False:
+                        print("Error! Make sure friend hasn't already been added or exists!")
+                        user_exist = True
+                        break
+            if manage_choice == '3':
+                current_userid = input("What is your name? ")
+                current_password = input("What is your password? ")
+                found_object = False
+                for person3 in social_network:
+                    if (person3.user_id == current_userid) and (person3.password == current_password):
+                        for element in person3.friendlist:
+                            print(element)
+                        found_object = True
+                if found_object == False:
+                    print("Couldn't find user!")
+            if manage_choice == '4':
+                current_userid = input("What is your name? ")
+                current_password = input("What is your password? ")
+                blocked_friend = input("What is the name of the user you'd like to block? ")
+                found_blocked = False
+                for index2 in social_network:
+                    if (index2.user_id == current_userid) and (index2.password == current_password):
+                        for friend in index2.friendlist:
+                            if blocked_friend == friend:
+                                index2.friendlist.remove(friend)
                                 print("Friend Blocked!")
                                 found_blocked = True
-                                break
-                    if not found_blocked:
-                        print("Couldn't find user or incorrect credentials!")
-                
-                elif manage_choice == '5':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    found_recipient = False
-                    sender_object = None
-                    for person1 in ai_social_network:
-                        if (person1.user_id == current_userid) and (person1.password == current_password):
-                            sender_object = person1
-                            message = input("What is the message you'd like to send? ")
-                            recipient = input("What is the username of the recipient? ")
-                            for person2 in ai_social_network:
-                                if (person2.user_id == recipient):
-                                    person2.send_message(f'{current_userid}: {message}')
-                                    person1.all_sent_messages(f'{current_userid}: {message}')
-                                    print("Message sent successfully!")
-                                    found_recipient = True
-                                    break
-                    if not found_recipient:
-                        print("Sorry! Could not send the message!")
-                
-                elif manage_choice == '6':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    found_user = False
-                    for person3 in ai_social_network:
-                        if (person3.user_id == current_userid) and (person3.password == current_password):
-                            found_user = True
-                            for message in person3.receivedmessages:
-                                print(message)
-                            break
-                    if not found_user:
-                        print("Couldn't find user!")
-                
-                elif manage_choice == '7':
-                    current_userid = input("What is your username? ")
-                    current_password = input("What is your password? ")
-                    found_user = False
-                    for person3 in ai_social_network:
-                        if (person3.user_id == current_userid) and (person3.password == current_password):
-                            found_user = True
-                            for message in person3.sentmessages:
-                                print(message)
-                            break
-                    if not found_user:
-                        print("Couldn't find user!")
-                
-        elif choice == '3':
-            break
+                        if found_blocked == False:
+                            print("Couldn't find user or incorrect credentials!")
+            if manage_choice == '5':
+                current_userid = input("What is your name? ")
+                current_password = input("What is your password? ")
+                found_recipient = False
+                for person1 in social_network:
+                    if (person1.user_id == current_userid) and (person1.password == current_password):
+                        sender = person1.user_id
+                        sender_object = person1
+                        message = input("What is the message you'd like to send? ")
+                        recipient = input("What is the name of the recipient? ")
+                        for person2 in social_network:
+                            if (person2.user_id == recipient):
+                                person2.send_message(f'{sender}: {message}')
+                                person1.all_sent_messages(f'{sender}: {message}')
+                                print("Message sent successfully!")
+                                found_recipient = True
+                if found_recipient == False:
+                    print("Sorry! Could not send message!")
+            if manage_choice == '6':
+                current_userid = input("What is your name? ")
+                current_password = input("What is your password? ")
+                found_user = False
+                for person3 in social_network:
+                    if (person3.user_id == current_userid) and (person3.password == current_password):
+                        found_user = True
+                        for message in person3.receivedmessages:
+                            print(message)
+                if found_user == False:
+                    print("Couldn't find user!")
+            if manage_choice == '7':
+                current_userid = input("What is your name? ")
+                current_password = input("What is your password? ")
+                found_user = False
+                for person3 in social_network:
+                    if (person3.user_id == current_userid) and (person3.password == current_password):
+                        found_user = True
+                        for message in person3.sentmessages:
+                            print(message)
+                if found_user == False:
+                    print("Couldn't find user!")
+    elif choice == '3':
+        break
